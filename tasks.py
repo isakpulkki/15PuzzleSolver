d91bac005d8f90c155234038bd0de958b0a25abd
isakpulkki <isak@pulkki.me>
2023-01-29 21:19:56 +0200

    Add instructions

--- tasks.py
+++ tasks.py
@@ -8,11 +8,3 @@ def start(ctx):
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
-
-@task
-def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src", pty=True)
-    
-@task(coverage)
-def coverage_report(ctx):
-    ctx.run("coverage html", pty=True)
