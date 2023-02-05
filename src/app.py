740f36577e176171bdb983c0a3baebcce11c4a20
isakpulkki <isak@pulkki.me>
2023-02-05 19:37:44 +0200

    Main app, switched to Tkinter

--- src/app.py
+++ src/app.py
@@ -1,50 +1,76 @@
-import pygame
-from puzzle import Puzzle
+import tkinter as tk
+from ui.menu import Menu
+from ui.board import Board
+from logic.puzzle import Puzzle
 
-pygame.init()
-pygame.display.set_caption("15PuzzleSolver")
 
-class Application:
+class Application(tk.Tk):
+    """
+    A class that represents a GUI layer for solving the 15-puzzle game using search algorithms.
+    It creates the GUI window and displays the puzzle board and the menu bar.
+    """
+
     def __init__(self):
-        """Initializes the components for the application, like fonts etc.
         """
-        pixels = 720
-        self.puzzle = Puzzle()
-        self.puzzle.set_numbers()
-        self.puzzle.shuffle_board(999)
-        self.rectSize = pixels / self.puzzle.size
-        self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((pixels, pixels))
-        self.screen.fill((0, 0, 0))
-        self.font = pygame.font.SysFont("", 68)
-        self.border_color = (35, 35, 35)
-        self.tile_color = (70, 70, 70)
-        self.font_color = (255, 255, 255)
-
-    def game_loop(self):
-        """The main loop of the application."""
-        while True:
-            self.draw_puzzle()
-            pygame.display.flip()
-            self.clock.tick(120)
-
-    def draw_puzzle(self):
-        """Draws a puzzle to the screen, forms rectangles for the numbers etc.
+        Initializes the Tkinter window, creates an instance of the Puzzle class, sets the puzzle numbers,
+        shuffles the board, creates instances of the Menu and Board classes etc.
         """
+        tk.Tk.__init__(self)
+        self.iconphoto(False, tk.PhotoImage(file='src/static/puzzle.png'))
+        self.title('15PuzzleSolver')
+        puzzle = Puzzle()
+        puzzle.set_numbers()
+        puzzle.shuffle_board(999)
+        self.puzzle = puzzle
+        self.grid()
+        self.width = 650
+        self.menu = Menu(self.width, 40, self)
+        self.init_menu()
+        self.board = Board(self.width, self)
+        self.board.init_board(self.puzzle)
+        self.moves = 0
+        self.mainloop()
+
+    def init_menu(self):
+        """
+        Draws the buttons and the moves label on the menu bar.
+        """
+        self.menu.draw_button(0, "Shuffle", lambda: [self.puzzle.shuffle_board(
+            999), self.reset_moves(), self.update()])
+        self.menu.draw_button(1, "Help", None)
+        self.menu.draw_moves(2)
+        self.menu.draw_button(3, "Solve", None)
+        self.menu.draw_button(4, "Exit", self.destroy)
+
+    def reset_moves(self):
+        """
+        Resets the number of moves to 0.
+        """
+        self.moves = 0
+
+    def update_number(self, x, y):
+        """
+        Attempts to move the number at the given position (x, y) in the puzzle and updates the number of moves.
+        Args:
+            x (int): x-coordinate of the number to be moved.
+            y (int): y-coordinate of the number to be moved.
+        """
+        direction = [x - self.puzzle.pos[0],
+                     y - self.puzzle.pos[1]]
+        if self.puzzle.move_number(direction):
+            self.moves += 1
+        self.update()
+
+    def update(self):
+        """
+        Updates the number of moves displayed on the menu bar and the state of the puzzle board.
+        """
+        self.menu.update_moves(self.moves)
         for x in range(self.puzzle.size):
             for y in range(self.puzzle.size):
-                text = str(self.puzzle.get_number(x, y))
-                rectangle = pygame.Rect(x * self.rectSize,
-                                        y * self.rectSize,
-                                        self.rectSize, self.rectSize)
-                if not text:
-                    pygame.draw.rect(self.screen, self.border_color, rectangle)
-                else:
-                    pygame.draw.rect(self.screen, self.tile_color, rectangle)
-                pygame.draw.rect(self.screen, self.border_color, rectangle, 1)
-                number = self.font.render(text, 1, self.font_color)
-                centered_width = self.rectSize / 2 - number.get_width() / 2
-                centered_height = (self.rectSize / 2) - number.get_height() / 2
-                self.screen.blit(number,
-                                 ((x * self.rectSize + centered_width), 
-                                 y * self.rectSize + centered_height))
+                number = self.puzzle.get_number(x, y)
+                self.board.update_piece(x, y, number)
+
+
+if __name__ == "__main__":
+    Application()
