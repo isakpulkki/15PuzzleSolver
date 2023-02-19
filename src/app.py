5cc833e88c8c65067af3b173eecdd1129de986b0
isakpulkki <isak@pulkki.me>
2023-02-19 22:34:57 +0200

    Solve the Puzzle using IDA*

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from ui.menu import Menu
 from ui.board import Board
 from logic.puzzle import Puzzle
+from logic.ida import IDAStar
+from static.style import WHITE, GREEN, BLACK
 
 
 class Application(tk.Tk):
@@ -18,11 +20,13 @@ class Application(tk.Tk):
         tk.Tk.__init__(self)
         self.iconphoto(False, tk.PhotoImage(file='src/static/puzzle.png'))
         self.title('15PuzzleSolver')
-        puzzle = Puzzle()
-        puzzle.set_numbers()
-        puzzle.shuffle_board(999)
-        self.puzzle = puzzle
+        self.resizable(False, False)
+        self.configure(background=BLACK)
+        self.puzzle = Puzzle()
+        self.puzzle.set_numbers()
+        self.puzzle.shuffle_board(999)
         self.grid()
+        self.idastar = IDAStar(self.puzzle)
         self.width = 650
         self.menu = Menu(self.width, 40, self)
         self.init_menu()
@@ -35,12 +39,10 @@ class Application(tk.Tk):
         """
         Draws the buttons and the moves label on the menu bar.
         """
-        self.menu.draw_button(0, "Shuffle", lambda: [self.puzzle.shuffle_board(
+        self.menu.draw_button(1, "Shuffle", lambda: [self.puzzle.shuffle_board(
             999), self.reset_moves(), self.update()])
-        self.menu.draw_button(1, "Help", None)
         self.menu.draw_moves(2)
-        self.menu.draw_button(3, "Solve", None)
-        self.menu.draw_button(4, "Exit", self.destroy)
+        self.menu.draw_button(3, "Solve", self.solve)
 
     def reset_moves(self):
         """
@@ -48,7 +50,7 @@ class Application(tk.Tk):
         """
         self.moves = 0
 
-    def update_number(self, x, y):
+    def update_number(self, x: int, y: int):
         """
         Attempts to move the number at the given positionition (x, y) in the puzzle and updates the number of moves.
         Args:
@@ -61,15 +63,33 @@ class Application(tk.Tk):
             self.moves += 1
         self.update()
 
+    def solve(self):
+        """
+        Solves the current Puzzle with IDA* algorithm.
+        """
+        self.idastar.puzzle = self.puzzle
+        if directions := self.idastar.start():
+            var = tk.IntVar(self)
+            for direction in directions:
+                self.puzzle.move_number(direction)
+                self.moves += 1
+                self.update()
+                self.after(500, var.set, 1)
+                self.wait_variable(var)
+
     def update(self):
         """
         Updates the number of moves displayed on the menu bar and the state of the puzzle board.
         """
+        color = WHITE
         self.menu.update_moves(self.moves)
+        if self.puzzle.is_solved():
+            self.moves = 0
+            color = GREEN
         for x in range(self.puzzle.size):
             for y in range(self.puzzle.size):
                 number = self.puzzle.get_number(x, y)
-                self.board.update_piece(x, y, number)
+                self.board.update_piece(x, y, number, color)
 
 
 if __name__ == "__main__":
