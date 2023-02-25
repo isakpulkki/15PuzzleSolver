a60bef7b7b92f0451488b413f9d90aab9b799ae0
isakpulkki <isak@pulkki.me>
2023-02-25 19:54:35 +0200

    Add Pylint checking to tasks

--- tasks.py
+++ tasks.py
@@ -3,11 +3,15 @@ from invoke import task
 @task
 def start(ctx):
     ctx.run("python3 src/app.py", pty=True)
-
+    
 @task
 def test(ctx):
-    ctx.run("python3 -m pytest src", pty=True)
+    ctx.run("pytest src/tests", pty=True)
 
 @task
 def build(ctx):
-    ctx.run("python3 -m src.logic.builder", pty=True)
+    ctx.run("python3 src/logic/builder.py", pty=True)
+
+@task
+def pylint(ctx):
+    ctx.run("poetry run pylint src", pty=True)
