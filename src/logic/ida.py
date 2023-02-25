6812e9fc11c16d752d80a5e21e508ab107bd0ce0
isakpulkki <isak@pulkki.me>
2023-02-25 19:57:24 +0200

    Refactor code

--- src/logic/ida.py
+++ src/logic/ida.py
@@ -1,34 +1,38 @@
-from logic.puzzle import Puzzle
 import pickle
 from time import time
+from src.logic.puzzle import Puzzle
+
 
 class IDAStar:
     def __init__(self, puzzle):
-        """Intializes the IDA* algorithm heuristics, from the file with the patterns and their weights.
-        """      
+        """Intializes the IDA* algorithm heuristics, from the file with patterns & their weights.
+        """
         self.puzzle = puzzle
         try:
             with open("patterns.dat", "rb") as file:
                 self.groups = pickle.load(file)
                 self.patterns = pickle.load(file)
                 if sum(len(group) for group in self.groups) != self.puzzle.size ** 2 - 1:
-                    print("Cannot perform IDA* to solve the Puzzle, because the additive pattern database does not match with this Puzzle.")
+                    print(
+                        "Cannot perform IDA* to solve the Puzzle, because additive pattern database" +
+                        " does not match with this Puzzle.")
                     self.groups = None
                 else:
-                    print("To solve the Puzzle, IDA* algorithm will use the additive pattern database for heuristics, with the groupings of "
-              f"({', '.join(str(len(group)) for group in self.groups)}).")
+                    print("To solve the Puzzle, IDA* algorithm will use the additive pattern" +
+                          " database for heuristics, with the groupings of " +
+                          f"({', '.join(str(len(group)) for group in self.groups)}).")
         except FileNotFoundError:
             print("No 'patterns.dat' found, try to rebuild the pattern database.")
             self.groups = None
-            
 
     def start(self):
-        """If the groups have been assigned, initializes the starting position of the algorithm, and the required data structures. 
-        Then, until the Puzzle is solved, the algorithm will iterate with the new bound values found.
+        """If the groups have been assigned, initializes the starting position of the algorithm,
+        and the required data structures. Then, until the Puzzle is solved, the algorithm will
+        iterate with the new bound values found.
 
         Returns:
             list: List of directions to solve the Puzzle.
-        """        
+        """
         if not self.groups:
             return False
         start = time()
@@ -37,27 +41,31 @@ class IDAStar:
         while True:
             t = self.search(path, directions, 0, bound)
             if t is True:
-                print(f"The algorithm found the path in {round(time() - start, 2)} seconds.")
+                print(
+                    f"The algorithm found the path in {round(time() - start, 2)} seconds.")
                 return directions
             bound = t
-            
+
     def search(self, path: list, directions: list, g: int, bound: int):
-        """Recursively finds the optimal path to the goal state using the IDA* (Iterative Deepening A*) algorithm. 
-    The algorithm takes the last puzzle in the path and calculates its 'f' value (moves + heuristic). 
-    If this value exceeds the given bound, the algorithm backtracks and returns the minimum possible bound from 
-    this path. If all the directions exceed the bound, the bound is updated and the search is repeated with the new bound. 
-    If the goal state is reached, the recursion terminates and returns True. 
+        """Recursively finds the optimal path to the goal state using IDA* (Iterative Deepening A*)
+        algorithm. The algorithm takes the last puzzle in the path and calculates its 'f' value
+        (moves + heuristic).
+        If this value exceeds the given bound, the algorithm backtracks and returns the minimum
+        possible bound from this path.
+        If all the directions exceed the bound, the bound is updated and the search is repeated
+        with the new bound. If the goal state is reached, the recursion terminates and returns True.
 
         Args:
         path (list): A list of Puzzles representing the current path.
-        directions (list): A list of directions taken to reach the current state from the previous states.
+        directions (list): A list of directions taken to reach the current state
+        from the previous states.
         g (int): The moves used to reach the current state from the initial state.
         bound (int): The maximum value of moves to reach the goal state.
 
     Returns:
-        bool: If the goal state is reached, returns True. 
+        bool: If the goal state is reached, returns True.
         min_bound: Otherwise, returns the minimum possible bound for the path.
-    """     
+    """
         puzzle: Puzzle = path[-1]
         f = g + self.heuristic(puzzle)
         if f > bound:
@@ -89,10 +97,9 @@ class IDAStar:
 
         Returns:
             bound: The moves needed to get into solved state from this Puzzle.
-        """        
+        """
         bound = 0
-        for g in range(len(self.groups)):
-            hashed_puzzle = puzzle.hash(self.groups[g])
-            self.patterns[g].get(hashed_puzzle)
-            bound += self.patterns[g].get(hashed_puzzle)
+        for i, group in enumerate(self.groups):
+            hashed_puzzle = puzzle.hash(group)
+            bound += self.patterns[i][hashed_puzzle]
         return bound
