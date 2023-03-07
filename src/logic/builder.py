fd021c24bb3776cfaceb8efd2726a72f697372dc
isakpulkki <isak@pulkki.me>
2023-03-07 02:27:00 +0200

    Fix tests

--- src/logic/builder.py
+++ src/logic/builder.py
@@ -3,7 +3,6 @@ from collections import deque
 from multiprocessing import Pool
 from src.logic.puzzle import Puzzle
 
-
 class PatternBuilder():
     def __init__(self,  group: dict):
         """Intializes the pattern database builder with Puzzle, sets it numbers without shuffling.
@@ -18,26 +17,24 @@ class PatternBuilder():
         self.group = group
         self.blank_group = group.copy()
         self.blank_group.add(0)
+        self.visited_list = set()
+        self.closed_list = {}
+        self.open_list = deque()
 
     def build_patterns(self):
-        """The method starts with the solved Puzzle and applies BFS to all possible permutations of
-        the Puzzle within the group. It uses a queue to store the current permutation & its previous
-        direction. The method checks if the permutation has been visited before, if not, adds
-        it to the visited set. It then simulates the Puzzle to every direction, creates a new
-        permutation only if the moved piece is in the group. The move is added to the permutation
-        only if the other moves belong to the same group.
+        """Applies BFS to all permutations of the Puzzle within the group, starting from 
+        the solved state. Uses a queue to store each permutation and its previous direction. 
+        Checks for previously visited permutations and adds new ones only if the moved piece 
+        is in the group and the other moves are in the same group.
 
         Returns:
             dict: This is the closed list with all of the permutations of the group.
         """
-        visited = set()
-        closed_list = {}
-        open_list = deque()
-        open_list.append((self.puzzle, [0, 0]))
+        self.open_list.append((self.puzzle, [0, 0]))
 
-        while open_list:
-            puzzle, previous = open_list.popleft()
-            if self.visit(puzzle, visited, closed_list):
+        while self.open_list:
+            puzzle, previous = self.open_list.popleft()
+            if self.visit(puzzle):
                 for direction in self.puzzle.directions:
                     if direction != previous:
                         simulated = puzzle.simulate(direction)
@@ -45,46 +42,38 @@ class PatternBuilder():
                             continue
                         if simulated[puzzle.position[0]][puzzle.position[1]] in self.group:
                             simulated.moves = puzzle.moves + 1
-                        open_list.append(
+                        self.open_list.append(
                             (simulated, [-direction[0], -direction[1]]))
 
         print(f"Group ({str(self.group)[1:-1]}) completed.")
-        return closed_list
+        return self.closed_list
 
-    def visit(self, puzzle: Puzzle, visited: set, closed_list: dict):
-        """Check if this permutation of the Puzzle with the blank tile
-        has already been visited. If not, set hash of the Puzzle without
-        the blank tile to the closed list, and give it the value how many
-        times the Puzzle has been moved in this Puzzle.
-
-        Args:
-            puzzle (Puzzle): This is the permutation of the Puzzle.
-            visited (set): Visited Puzzles with the blank piece.
-            closed_list (dict): List of closed permutations.
+    def visit(self, puzzle: Puzzle):
+        """Checks if permutation with the blank tile has been visited. 
+        If not, adds Puzzle without blank tile to closed list with the number of moves made.
 
         Returns:
-            bool: If the permutation has not been visited.
+            bool: If the permutation has not been self.visited_list.
         """
         hashed_blank_puzzle = puzzle.hash(self.blank_group)
-        if hashed_blank_puzzle in visited:
+        if hashed_blank_puzzle in self.visited_list:
             return False
 
-        visited.add(hashed_blank_puzzle)
+        self.visited_list.add(hashed_blank_puzzle)
 
         hashed_puzzle = puzzle.hash(self.group)
-        if hashed_puzzle not in closed_list or closed_list[hashed_puzzle] > puzzle.moves:
-            closed_list[hashed_puzzle] = puzzle.moves
+        if hashed_puzzle not in self.closed_list or self.closed_list[hashed_puzzle] > puzzle.moves:
+            self.closed_list[hashed_puzzle] = puzzle.moves
 
         return True
-
 
 def main():
     """This is the main function of the pattern builder. You can set different groupings
     if you want to. The patterns are independent, so they can be assigned to their own
     processes. This function also stores the pattern data to a file for later use.
     """
-    patterns = [PatternBuilder({1, 5, 6, 9, 10, 13}), PatternBuilder({7, 8, 11, 12, 14, 15}),
-                PatternBuilder({2, 3, 4})]
+    patterns = [PatternBuilder({1, 2, 3, 4, 7}), PatternBuilder({5, 6, 9, 10, 13}),
+                PatternBuilder({8, 11, 12, 14, 15})]
     closed_list = []
 
     print("Generating patterns, this could take a while...")
